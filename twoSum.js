fca39bd7a6531a1ca81ba68f06c636ff85950fe6
Arthur Kateeba <arthurkateeba@gmail.com>
2023-10-22 18:27:35 +0300

    update nested loop in function

--- twoSum.js
+++ twoSum.js
@@ -8,8 +8,10 @@ const twoSum = (nums, target) => {
   for (let i = 0; i < copyOfInputArray.length; i++) {
   //check if sum of any two items in array sums up to the target;
       for (j = 0; j < inputArray.length; j++) {
-          if((arr[i] + arr[j] === sumOfAnyTwoItemsOfNums) && (arr[i] + arr[i] === sumOfAnyTwoItemsOfNums))
+          if((arr[i] + arr[j] === sumOfAnyTwoItemsOfNums) && !(arr[i] + arr[i] === sumOfAnyTwoItemsOfNums)) {
+            return `${arr[i]} + ${arr[j]}`;
+        }
     }
   }
-}
+};
 
