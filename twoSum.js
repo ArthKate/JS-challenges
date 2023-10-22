18c2a0a45807049003b94fa5d80471731e3c1d46
Arthur Kateeba <arthurkateeba@gmail.com>
2023-10-22 16:25:38 +0300

    create twoSum JS solution

--- twoSum.js
+++ twoSum.js
@@ -1,3 +1,15 @@
-const twoSum = (nums, target) => {
 
+const twoSum = (nums, target) => {
+  const inputArray = nums;
+  const copyOfInputArray = [...inputArray];
+  let sumOfAnyTwoItemsOfNums = target;
+
+  //loop through the nums array items
+  for (let i = 0; i < copyOfInputArray.length; i++) {
+  //check if sum of any two items in array sums up to the target;
+      for (j = 0; j < inputArray.length; j++) {
+          if((arr[i] + arr[j] === sumOfAnyTwoItemsOfNums) && (arr[i] + arr[i] === sumOfAnyTwoItemsOfNums))
+    }
+  }
 }
+
