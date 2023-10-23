75f35163703b631098fca0b1f0fb2f4d18c3ab97
Arthur Kateeba <arthurkateeba@gmail.com>
2023-10-23 15:36:35 +0300

    remove nested loop in solution

--- returnObj.js
+++ returnObj.js
@@ -8,35 +8,24 @@ const returnObj = (arrInput) => {
   // const divisibleByTwo = new Array();
   //loop through the items in the array
 
-  for (let item of copyOfArr) {
-    for (let i = 0; i < arr.length; i++) {
-      if (copyOfArr[item] < arr[i] && copyOfArr[item] !== arr[i]) {
-        arr[arr.indexOf(item)] = arr[i];
-        arr[i] = item;
-      }
-    }
-    return arr;
-  }
-
-  for (let item of arr) {
+  for (let i of arr) {
     // check if item is either even, odd of char
-    if (typeof item === 'string') {
+    if (typeof i === 'string') {
       // check for item with string data type
-      newObj.chars.push(item);
+      newObj.chars.push(i);
     }
-    if (typeof item === 'number') {
+    if (typeof i === 'number') {
       // check for item of number data type
-      if (item % 2 === 0) {
-        newObj.evens.push(item);
+      if (i % 2 === 0) {
+        newObj.evens.push(i);
       } else {
-        newObj.odds.push(item);
+        newObj.odds.push(i);
       }
     }
   }
   return newObj;
-};
+}
 
+const arrInput = [3.0, 'a', 7, 'x', 20, 'd', 4, 'f', 8]
 
-const arr = [3.0, 'a', 7, 'x', 20, 'd', 4, 'f', 8]
-
-console.log(returnObj(arr));
+console.log(returnObj(arrInput));
